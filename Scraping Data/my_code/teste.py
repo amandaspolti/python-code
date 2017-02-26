a280170ab2698ab89c5db790acdbaf4fd73fbb65
Amanda Spolti <acostasp@oswego.edu>
2017-02-26 16:30:43 -0300

    Add files for scraping data from Facebook created by minimaxir Add file containing collected information from the facebook page joaopamg

--- Scraping Data/my_code/teste.py
+++ Scraping Data/my_code/teste.py
@@ -8,7 +8,7 @@ links = soup.find_all("a")
 
 for link in links:
     #if "http" in link:
-    print "<a href='%s'>%s</a>" %(link.get("href"), link.text)
+    print ("<a href='%s'>%s</a>" %(link.get("href"), link.text))
     #or > link.get("href")
     #or print link.text // print link.text, link.get("href")
     #"<a href='%s'>%s</a>" %(link.get("href"), link.text)
@@ -16,9 +16,9 @@ for link in links:
 g_data = soup.find_all("div", {"class": "info"})
 
 for item in g_data:
-    print item.contents[0].find_all("a", {"class": "business-name"})[0].text
-    print item.contents[1].find_all("p", {"class": "adr"})[0].text
+    print (item.contents[0].find_all("a", {"class": "business-name"})[0].text)
+    print (item.contents[1].find_all("p", {"class": "adr"})[0].text)
     try:
-        print item.contents[1].find_all("li", {"class": "primary"})[0].text
+        print (item.contents[1].find_all("li", {"class": "primary"})[0].text)
     except:
         pass
